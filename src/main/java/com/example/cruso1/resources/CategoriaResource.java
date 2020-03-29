1f3726861b6597eebe6fca3e095cfb1ecffb7d30
Fabiofcn <fabiofcn@gmail.com>
2020-03-29 10:24:53 -0300

    Testando Classe domio categoria

--- src/main/java/com/example/cruso1/resources/CategoriaResource.java
+++ src/main/java/com/example/cruso1/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.example.cruso1.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cruso1.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> Listar() {
+		Categoria cat1 =new Categoria(1,"informatica");
+		Categoria cat2 =new Categoria(2,"escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 }
