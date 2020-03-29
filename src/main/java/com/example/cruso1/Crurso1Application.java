8efd05b4d5d0b83a3f3cf8ebd558cf1e3acfc658
Fabiofcn <fabiofcn@gmail.com>
2020-03-29 09:35:25 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/cruso1/Crurso1Application.java
@@ -0,0 +1,14 @@
+package com.example.cruso1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+public class Crurso1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crurso1Application.class, args);
+	}
+
+}
